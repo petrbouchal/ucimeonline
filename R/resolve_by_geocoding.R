559fdc797ce69a0713267b5ae9fea78ad18766a5
Petr Bouchal <pbouchal@gmail.com>
2021-03-08 23:41:33 +0100

    cleanup

--- R/resolve_by_geocoding.R
+++ R/resolve_by_geocoding.R
@@ -7,6 +7,5 @@ at_still_unresolved <- at_with_geo %>%
   filter(!id %in% resolved$id) %>%
   select(red_izo, nazev_skoly, adresa_lower, id)
 
-# Match to school (not just org) ------------------------------------------
 
 
