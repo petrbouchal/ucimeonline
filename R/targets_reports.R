df4bb7d56df005544c00876c8fd14525c040cdb6
Petr Bouchal <pbouchal@gmail.com>
2021-03-11 14:07:03 +0100

    improve site rendering

--- R/targets_reports.R
+++ R/targets_reports.R
@@ -5,33 +5,29 @@ t_reports <- list(
 
   tar_file(rmd_index, "index.Rmd"),
   tar_file(html_index, command = {!! tar_knitr_deps_expr("index.Rmd")
-    rmd_index
     siteconf
     sitefiles
-    rmarkdown::render_site("index.Rmd")
+    rmarkdown::render_site(rmd_index)
     "docs/index.html"}),
 
   tar_file(rmd_doc, "README.Rmd"),
   tar_file(html_doc, command = {!! tar_knitr_deps_expr("README.Rmd")
-    rmd_doc
     siteconf
     sitefiles
-    rmarkdown::render_site("README.Rmd")
+    rmarkdown::render_site(rmd_doc)
     "docs/README.html"}),
 
   tar_file(rmd_codebook, "codebook.Rmd"),
-  tar_file(html_codebook, command = {!! tar_knitr_deps_expr("index.Rmd")
-    rmd_index
+  tar_file(html_codebook, command = {!! tar_knitr_deps_expr("codebook.Rmd")
     siteconf
     sitefiles
-    rmarkdown::render_site("codebook.Rmd")
+    rmarkdown::render_site(rmd_codebook)
     "docs/codebook.html"}),
 
   tar_file(rmd_dev, "dev.Rmd"),
   tar_file(html_dev, command = {!! tar_knitr_deps_expr("dev.Rmd")
-    rmd_index
     siteconf
     sitefiles
-    rmarkdown::render_site("dev.Rmd")
-    "docs/dev.html"})
+    rmarkdown::render_site(rmd_dev)
+    "docs/dev.html"}),
 )
